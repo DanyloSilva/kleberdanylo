9b8e4822f46779226a5717629cdee037c0dffcb6
Danylo Silva <72108514+DanyloSilva@users.noreply.github.com>
2021-06-26 17:01:24 -0300

    fist commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("tudo e um e um e tudo");
+System.out.println("Mas a soma de tudo e 0");
+System.out.println("tudo e um e um e tudo");
+
+	}
+
+}
